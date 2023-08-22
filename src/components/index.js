f8d6be272d8610cb22fd76554f53c387c8957cd3
uzohou <isaiahntina47@gmail.com>
2023-08-22 01:28:00 +0100

    history section, attendance sheet

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import DashboardComponent from '@/components/Dashboard';
+import ClassDetailsPage from '@/components/ClassDetails';
+import AttendanceReport from '@/components/attendancereport';
+
+export {
+    Navbar,
+    Sidebar,
+    DashboardComponent,
+    ClassDetailsPage,
+    AttendanceReport
+  };
