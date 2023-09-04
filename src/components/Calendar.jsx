f0b053463193b2580e59a7ae2fd618c79068d910
uzohou <isaiahntina47@gmail.com>
2023-09-04 12:09:22 +0100

    calender upgrade

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,7 +4,9 @@ import { startOfMonth, endOfMonth, eachDayOfInterval, format, getDay } from 'dat
 const Calendar = ({ classes }) => {
   // Dummy data for demonstration (replace with your actual data)
   const dummyClasses = [
-    { date: '2023-09-04', name: 'Class A_MON' },
+
+
+{ date: '2023-09-04', name: 'Class A_MON' },
 { date: '2023-09-04', name: 'Class B_MON' },
 { date: '2023-09-04', name: 'Class C_MON' },
 { date: '2023-09-04', name: 'Class D_MON' },
@@ -38,14 +40,13 @@ const Calendar = ({ classes }) => {
 { date: '2023-09-20', name: 'Class A_WED' },
 { date: '2023-09-20', name: 'Class B_WED' },
 { date: '2023-09-21', name: 'Class A_THUR' },
-{ date: '2023-09-22', name: 'Class A_FRI' },
   ];
 
   // Generate an array of days for the current month
   const currentDate = new Date();
   const startOfMonthDate = startOfMonth(currentDate);
   const endOfMonthDate = endOfMonth(currentDate);
-  const daysOfMonth = eachDayOfInterval({ start: startOfMonthDate, end: endOfMonthDate });
+  const daysOfMonth = eachDayOfInterval({ start:startOfMonthDate, end:endOfMonthDate });
 
   // Organize classes by day
   const classesByDay = {};
@@ -84,10 +85,15 @@ const Calendar = ({ classes }) => {
         const dayClasses = classesByDay[formattedDay] || [];
         const dayOfWeek = getDay(day); // Get the day of the week directly from the date
 
+        // Highlight current day
+        const isToday = formattedDay === format(new Date(), "yyyy-MM-dd");
+        const cellBgColor = isToday ? "bg-green-100" : "";
+
         // Render only weekdays (exclude weekends)
         if (dayOfWeek > 0 && dayOfWeek < 6) {
           return (
-            <div key={formattedDay} className="p-2 text-center border border-red">
+            <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`}>
+              <div className="text-xs text-gray-400">{format(day, "d")}</div>
               {dayClasses.map((classItem) => (
                 <div key={classItem.name} className="text-xs font-semibold bg-blue-100 rounded-full px-2 py-1 my-1">
                   {classItem.name}
@@ -96,7 +102,7 @@ const Calendar = ({ classes }) => {
             </div>
           );
         } else {
-          return <div key={formattedDay} className="p-2 text-center border border-gray-300" />;
+          return <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`} />;
         }
       })}
     </div>
@@ -104,3 +110,41 @@ const Calendar = ({ classes }) => {
 };
 
 export default Calendar;
+
+
+
+
+// { date: '2023-09-04', name: 'Class A_MON' },
+// { date: '2023-09-04', name: 'Class B_MON' },
+// { date: '2023-09-04', name: 'Class C_MON' },
+// { date: '2023-09-04', name: 'Class D_MON' },
+// { date: '2023-09-05', name: 'Class A_TUE' },
+// { date: '2023-09-05', name: 'Class B_TUE' },
+// { date: '2023-09-05', name: 'Class C_TUE' },
+// { date: '2023-09-06', name: 'Class A_WED' },
+// { date: '2023-09-06', name: 'Class B_WED' },
+// { date: '2023-09-07', name: 'Class A_THUR' },
+// { date: '2023-09-08', name: 'Class A_FRI' },
+// { date: '2023-09-08', name: 'Class B_FRI' },
+// { date: '2023-09-11', name: 'Class A_MON' },
+// { date: '2023-09-11', name: 'Class B_MON' },
+// { date: '2023-09-11', name: 'Class C_MON' },
+// { date: '2023-09-11', name: 'Class D_MON' },
+// { date: '2023-09-12', name: 'Class A_TUE' },
+// { date: '2023-09-12', name: 'Class B_TUE' },
+// { date: '2023-09-12', name: 'Class C_TUE' },
+// { date: '2023-09-13', name: 'Class A_WED' },
+// { date: '2023-09-13', name: 'Class B_WED' },
+// { date: '2023-09-14', name: 'Class A_THUR' },
+// { date: '2023-09-15', name: 'Class A_FRI' },
+// { date: '2023-09-15', name: 'Class B_FRI' },
+// { date: '2023-09-18', name: 'Class A_MON' },
+// { date: '2023-09-18', name: 'Class B_MON' },
+// { date: '2023-09-18', name: 'Class C_MON' },
+// { date: '2023-09-18', name: 'Class D_MON' },
+// { date: '2023-09-19', name: 'Class A_TUE' },
+// { date: '2023-09-19', name: 'Class B_TUE' },
+// { date: '2023-09-19', name: 'Class C_TUE' },
+// { date: '2023-09-20', name: 'Class A_WED' },
+// { date: '2023-09-20', name: 'Class B_WED' },
+// { date: '2023-09-21', name: 'Class A_THUR' },
