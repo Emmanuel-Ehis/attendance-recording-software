8a3fc0af01ca1b0d3585bcae4f56b33d6a18ea7a
Jared Momanyi <jjjmomanyi@gmail.com>
2023-09-06 00:20:02 +0100

    calendar works needs a UI fix

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,46 +1,66 @@
 import React from 'react';
 import { startOfMonth, endOfMonth, eachDayOfInterval, format, getDay } from 'date-fns';
+import User from '@/DB/User';
+import supabase from '@/DB/Client';
+import  { useEffect, useState } from 'react';
 
 const Calendar = ({ classes }) => {
-  // Dummy data for demonstration (replace with your actual data)
-  const dummyClasses = [
+  const [dummyClasses, setDummyClasses] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedSession, setSelectedSession] = useState(null);
+
+  async function fetchSessions() {
+    try {
+      const userID = await User();
+      const currentDate = new Date();
+      const startDate = format(startOfMonth(currentDate), 'yyyy-MM-dd');
+      const endDate = format(endOfMonth(currentDate), 'yyyy-MM-dd');
+      const { data, error } = await supabase
+        .from('Sessions')
+        .select(`*,Subjects(Name, Initials)`)
+        .eq('userID', userID)
+        .gte('Date', startDate)
+        .lte('Date', endDate);
+
+      if (error) {
+        console.error(error);
+      } else {
+        const sessions = data.map((session) => ({
+          date: session.Date,
+          name: session.Subjects.Name,
+          starts: session.StartTime,
+          ends: session.EndTime,
 
 
-{ date: '2023-09-04', name: 'Class A_MON' },
-{ date: '2023-09-04', name: 'Class B_MON' },
-{ date: '2023-09-04', name: 'Class C_MON' },
-{ date: '2023-09-04', name: 'Class D_MON' },
-{ date: '2023-09-05', name: 'Class A_TUE' },
-{ date: '2023-09-05', name: 'Class B_TUE' },
-{ date: '2023-09-05', name: 'Class C_TUE' },
-{ date: '2023-09-06', name: 'Class A_WED' },
-{ date: '2023-09-06', name: 'Class B_WED' },
-{ date: '2023-09-07', name: 'Class A_THUR' },
-{ date: '2023-09-08', name: 'Class A_FRI' },
-{ date: '2023-09-08', name: 'Class B_FRI' },
-{ date: '2023-09-11', name: 'Class A_MON' },
-{ date: '2023-09-11', name: 'Class B_MON' },
-{ date: '2023-09-11', name: 'Class C_MON' },
-{ date: '2023-09-11', name: 'Class D_MON' },
-{ date: '2023-09-12', name: 'Class A_TUE' },
-{ date: '2023-09-12', name: 'Class B_TUE' },
-{ date: '2023-09-12', name: 'Class C_TUE' },
-{ date: '2023-09-13', name: 'Class A_WED' },
-{ date: '2023-09-13', name: 'Class B_WED' },
-{ date: '2023-09-14', name: 'Class A_THUR' },
-{ date: '2023-09-15', name: 'Class A_FRI' },
-{ date: '2023-09-15', name: 'Class B_FRI' },
-{ date: '2023-09-18', name: 'Class A_MON' },
-{ date: '2023-09-18', name: 'Class B_MON' },
-{ date: '2023-09-18', name: 'Class C_MON' },
-{ date: '2023-09-18', name: 'Class D_MON' },
-{ date: '2023-09-19', name: 'Class A_TUE' },
-{ date: '2023-09-19', name: 'Class B_TUE' },
-{ date: '2023-09-19', name: 'Class C_TUE' },
-{ date: '2023-09-20', name: 'Class A_WED' },
-{ date: '2023-09-20', name: 'Class B_WED' },
-{ date: '2023-09-21', name: 'Class A_THUR' },
-  ];
+
+        }));
+        setDummyClasses(sessions);
+       
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+//handle when a user clicks on a class
+  const handleSessionClick = (session) => {
+    setIsModalOpen(true);
+   const selected= dummyClasses.find((sessionItem) => sessionItem.name === session);
+
+    setSelectedSession(selected);
+  };
+
+  //close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedSession(null);
+  };
+
+
+
+  useEffect(() => {
+    fetchSessions();
+  }, []);
+
 
   // Generate an array of days for the current month
   const currentDate = new Date();
@@ -67,84 +87,69 @@ const Calendar = ({ classes }) => {
   // Calculate the day of the week for the first day of the month (0 for Sunday, 1 for Monday, etc.)
   const firstDayOfWeek = getDay(startOfMonthDate);
 
-  return (
-    <div className="grid grid-cols-7 gap-4 p-8 shadow-md md:ml-[4rem]">
-      {/* Render days of the week headers */}
-      {weekdays.map((day) => (
-        <div key={day} className="bg-gray-200 text-gray-800 p-2 text-center font-bold">
-          {day}
-        </div>
-      ))}
-      {/* Add empty cells before the first day of the month */}
-      {[...Array(firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1)].map((_, index) => (
-        <div key={`empty-${index}`} className="p-2 text-center border border-gray-300" />
-      ))}
-      {/* Fill in the calendar cells with class data */}
-      {daysOfMonth.map((day) => {
-        const formattedDay = format(day, "yyyy-MM-dd");
-        const dayClasses = classesByDay[formattedDay] || [];
-        const dayOfWeek = getDay(day); // Get the day of the week directly from the date
 
-        // Highlight current day
-        const isToday = formattedDay === format(new Date(), "yyyy-MM-dd");
-        const cellBgColor = isToday ? "bg-green-100" : "";
 
-        // Render only weekdays (exclude weekends)
-        if (dayOfWeek > 0 && dayOfWeek < 6) {
-          return (
-            <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`}>
-              <div className="text-xs text-gray-400">{format(day, "d")}</div>
-              {dayClasses.map((classItem) => (
-                <div key={classItem.name} className="text-xs font-semibold bg-blue-100 rounded-full px-2 py-1 my-1">
-                  {classItem.name}
-                </div>
-              ))}
+return (
+  <div className="grid grid-cols-7 gap-4 p-8 shadow-md md:ml-[4rem]">
+    {/* Render days of the week headers */}
+    {weekdays.map((day) => (
+      <div key={day} className="bg-gray-200 text-gray-800 p-2 text-center font-bold">
+        {day}
+      </div>
+    ))}
+    {/* Add empty cells before the first day of the month */}
+    {[...Array(firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1)].map((_, index) => (
+      <div key={`empty-${index}`} className="p-2 text-center border border-gray-300" />
+    ))}
+    {/* Fill in the calendar cells with class data */}
+    {daysOfMonth.map((day) => {
+      const formattedDay = format(day, "yyyy-MM-dd");
+      const dayClasses = classesByDay[formattedDay] || [];
+      const dayOfWeek = getDay(day); // Get the day of the week directly from the date
+
+      // Highlight current day
+      const isToday = formattedDay === format(new Date(), "yyyy-MM-dd");
+      const cellBgColor = isToday ? "bg-green-100" : "";
+
+      // Render only weekdays (exclude weekends)
+      if (dayOfWeek > 0 && dayOfWeek < 6) {
+        return (
+          <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`}>
+            <div className="text-xs text-gray-400">{format(day, "d")}</div>
+            {dayClasses.map((classItem) => (
+              <button  onClick={() => handleSessionClick(classItem.name)} key={classItem.name} className="text-xs font-semibold bg-blue-100 rounded-full px-2 py-1 my-1">
+                {classItem.name}
+              </button>
+            ))}
+          </div>
+        );
+      } else {
+        return <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`} />;
+      }
+    })}
+    {/* Modal for when a user clicks on a class */}
+    {isModalOpen && (
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="absolute" onClick={closeModal}></div>
+        <div className="bg-white p-4 rounded-lg shadow-md">
+          <h2 className="text-lg font-semibold">Class details</h2>
+          {selectedSession && (
+            <div>
+              <p className='Bold'>Name: {selectedSession.name}</p>
+              <p>Date: {selectedSession.date}</p>
+              <p>Class starts: {selectedSession.starts}</p>
+              <p>Class ends: {selectedSession.ends}</p>
+         
             </div>
-          );
-        } else {
-          return <div key={formattedDay} className={`p-2 text-center border ${cellBgColor}`} />;
-        }
-      })}
-    </div>
-  );
-};
+          )}
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4" onClick={closeModal}>
+            Close
+          </button>
+        </div>
+      </div>
+    )}
+  </div>
+);
+          }
 
 export default Calendar;
-
-
-
-
-// { date: '2023-09-04', name: 'Class A_MON' },
-// { date: '2023-09-04', name: 'Class B_MON' },
-// { date: '2023-09-04', name: 'Class C_MON' },
-// { date: '2023-09-04', name: 'Class D_MON' },
-// { date: '2023-09-05', name: 'Class A_TUE' },
-// { date: '2023-09-05', name: 'Class B_TUE' },
-// { date: '2023-09-05', name: 'Class C_TUE' },
-// { date: '2023-09-06', name: 'Class A_WED' },
-// { date: '2023-09-06', name: 'Class B_WED' },
-// { date: '2023-09-07', name: 'Class A_THUR' },
-// { date: '2023-09-08', name: 'Class A_FRI' },
-// { date: '2023-09-08', name: 'Class B_FRI' },
-// { date: '2023-09-11', name: 'Class A_MON' },
-// { date: '2023-09-11', name: 'Class B_MON' },
-// { date: '2023-09-11', name: 'Class C_MON' },
-// { date: '2023-09-11', name: 'Class D_MON' },
-// { date: '2023-09-12', name: 'Class A_TUE' },
-// { date: '2023-09-12', name: 'Class B_TUE' },
-// { date: '2023-09-12', name: 'Class C_TUE' },
-// { date: '2023-09-13', name: 'Class A_WED' },
-// { date: '2023-09-13', name: 'Class B_WED' },
-// { date: '2023-09-14', name: 'Class A_THUR' },
-// { date: '2023-09-15', name: 'Class A_FRI' },
-// { date: '2023-09-15', name: 'Class B_FRI' },
-// { date: '2023-09-18', name: 'Class A_MON' },
-// { date: '2023-09-18', name: 'Class B_MON' },
-// { date: '2023-09-18', name: 'Class C_MON' },
-// { date: '2023-09-18', name: 'Class D_MON' },
-// { date: '2023-09-19', name: 'Class A_TUE' },
-// { date: '2023-09-19', name: 'Class B_TUE' },
-// { date: '2023-09-19', name: 'Class C_TUE' },
-// { date: '2023-09-20', name: 'Class A_WED' },
-// { date: '2023-09-20', name: 'Class B_WED' },
-// { date: '2023-09-21', name: 'Class A_THUR' },
